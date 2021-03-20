cb155d15af2dd9e5c15b8632bedd52639a24c3c9
AlexanderPalencia <mapg23@outlook.com>
2021-03-20 10:59:35 -0600

    adding final test

--- Test_System_Calls_C/tcheckstring.c
+++ Test_System_Calls_C/tcheckstring.c
@@ -8,8 +8,11 @@
 int main(void){
 	
     char st[256];
-    sprintf(st, "(5)*(745))");
-    
+    printf("Enter the : ");
+    fgets(st, sizeof(st), stdin);  // read string
+    printf("Name: ");
+    puts(st);
+
     long sta = syscall(337, st, 20);
     //I type 20 simply because I don't want to do any math
     printf("return value from syscall: %ld\n", sta);
